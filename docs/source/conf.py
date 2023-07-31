0d6f985b4166ea2e6ece7e5d4684b22223a69de8
growtoups <67888122+growtoups@users.noreply.github.com>
2023-07-31 11:12:22 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Mycelium-API'
+copyright = '2023, BrainBytes'
+author = 'BrainBytes'
 
 release = '0.1'
 version = '0.1.0'
